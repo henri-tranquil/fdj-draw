03e41eca22535ea4c738477649c4f46cb343c6ba
henri-tranquil <henriletellier61@gmail.com>
2022-12-04 00:28:02 +0000

    New data added

--- scripts/download_latest.sh
+++ scripts/download_latest.sh
@@ -9,4 +9,4 @@ npm run fillData
 rm -rf ../resources/euromillions.csv
 git commit -m "New data added" -a && git push
 
-curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"Les données du loto on été mises à jour\"}" $DISCORD_HOOK
+curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"Les données du loto on été mises à jour\"}" https://discord.com/api/webhooks/1047987192607297566/I_zC3Wma4bLuwDxleVzwreGhkWqQrRdqg7ATJOdIIoUJudX5-G0cAeMUTt8Yvq81O95V
