d6b47e02a99917e6c85d7e085ca328bc8e2e8df0
henri-tranquil <henriletellier61@gmail.com>
2022-12-04 15:48:24 +0100

    Add hash system to keep the file up to date

--- api/src/euromillions/euromillions.service.ts
+++ api/src/euromillions/euromillions.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import * as fs from 'fs';
+import { createHash } from 'crypto';
+import { readFileSync } from 'fs';
 import { Euromillions } from './euromillions.dto';
 
 const RESOURCES_FOLDER = '../resources';
@@ -7,21 +8,34 @@ const RESOURCES_FOLDER = '../resources';
 @Injectable()
 export class EuromillionsService {
   data: Euromillions[];
+  latestHash: string;
   constructor() {
     console.log('PWD :', process.env.PWD);
-    this.data = JSON.parse(
-      fs.readFileSync(RESOURCES_FOLDER + '/euromillions.json', {
-        encoding: 'utf8',
-        flag: 'r',
-      }),
-    );
+    this.loadFile();
   }
 
   getAll() {
+    this.loadFile();
     return this.data;
   }
 
   getLatest() {
+    this.loadFile();
     return this.data[0];
+  }
+
+  loadFile() {
+    const bufferedFile = readFileSync(RESOURCES_FOLDER + '/euromillions.json', {
+      encoding: 'utf8',
+      flag: 'r',
+    });
+    const hashSum = createHash('sha256');
+    hashSum.update(bufferedFile);
+    const hash = hashSum.digest('hex');
+    if (hash !== this.latestHash) {
+      console.log('FILE HAS CHANGED');
+      this.latestHash = hash;
+      this.data = JSON.parse(bufferedFile);
+    }
   }
 }
