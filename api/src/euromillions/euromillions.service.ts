744eedfd4a3debaf2b87383974be050679f0a328
henri-tranquil <henriletellier61@gmail.com>
2023-05-11 00:01:08 +0200

    Add prediction

--- api/src/euromillions/euromillions.service.ts
+++ api/src/euromillions/euromillions.service.ts
@@ -4,6 +4,7 @@ import { readFileSync } from 'fs';
 import { Euromillions } from './euromillions.dto';
 import axios from 'axios';
 import { env } from 'process';
+import { NeuralNetwork } from 'brain.js';
 
 const RESOURCES_FOLDER = '../resources';
 
@@ -47,6 +48,92 @@ export class EuromillionsService {
     return false;
   }
 
+  predictNext() {
+    const data = [...this.data].reverse();
+
+    const processedData = data.map((d) => {
+      const date = new Date(d.date);
+      const balls = d.balls.map((n) => parseInt(n.toString()));
+      const stars = d.stars.map((s) => parseInt(s.toString()));
+      return { ...d, date, balls, stars };
+    });
+
+    // Préparer les données d'entraînement
+    const trainingBallsData = [];
+    const trainingstarsData = [];
+    for (let i = 1; i < processedData.length; i++) {
+      const element = processedData[i];
+      const last = processedData[i - 1];
+      const input_balls = this._generateOutput(last.balls);
+      const input_stars = this._generateOutput(last.stars, true);
+      const output_balls = this._generateOutput(element.balls);
+      const output_stars = this._generateOutput(element.stars, true);
+      trainingBallsData.push({ input_balls, output_balls });
+      trainingstarsData.push({ input_stars, output_stars });
+    }
+    console.log('INITIALISE NETWORK');
+
+    // Configurer le réseau de neurones
+    const net_balls = new NeuralNetwork({
+      hiddenLayers: [5, 3],
+    });
+
+    const net_stars = new NeuralNetwork({
+      hiddenLayers: [5, 3],
+    });
+
+    console.log('TRAIN NETWORK...');
+
+    // Entraîner le réseau de neurones
+    net_balls.train(trainingBallsData);
+    net_stars.train(trainingstarsData);
+
+    // Utiliser le réseau de neurones pour prédire les numéros du prochain tirage
+    const previousNumbersBalls = this._generateOutput(
+      processedData[processedData.length - 1].balls,
+    );
+    const previousNumbersStars = this._generateOutput(
+      processedData[processedData.length - 1].stars,
+      true,
+    );
+    const input_balls = previousNumbersBalls;
+    const input_stars = previousNumbersStars;
+
+    const output_balls = net_balls.run(input_balls);
+    const output_stars = net_balls.run(input_stars);
+    const result = {
+      balls: this._getBestFromResult(output_balls).sort(),
+      stars: this._getBestFromResult(output_stars, true).sort(),
+    };
+    console.log('TRAINING FINISHED');
+
+    return result;
+  }
+
+  _generateOutput(balls, is_stars = false) {
+    const nb_balls = is_stars ? 12 : 50;
+    const generatedOutput = {};
+    for (let i = 0; i < nb_balls; i++) {
+      generatedOutput[`b${(i + 1).toString()}`] = 0;
+    }
+
+    for (const ball of balls) {
+      generatedOutput[`b${ball.toString()}`] = 1;
+    }
+    return generatedOutput;
+  }
+
+  _getBestFromResult(results, is_stars = false) {
+    const nb_element = is_stars ? 2 : 5;
+    const sortedPairs = Object.entries(results).sort(
+      (a: any, b: any) => b[1] - a[1],
+    );
+
+    const sortedObj = Object.fromEntries(sortedPairs);
+    const best = Object.keys(sortedObj).slice(0, nb_element);
+    return best.map((e) => parseInt(e.substring(1)));
+  }
+
   loadFile() {
     const bufferedFile = readFileSync(RESOURCES_FOLDER + '/euromillions.json', {
       encoding: 'utf8',
@@ -58,6 +145,7 @@ export class EuromillionsService {
     if (hash !== this.latestHash) {
       this.latestHash = hash;
       this.data = JSON.parse(bufferedFile);
+      const nextDraw = this.predictNext();
       const embeds = [
         {
           title: `New Draw : ${new Date().toLocaleString()}`,
@@ -69,6 +157,19 @@ export class EuromillionsService {
             {
               name: `Stars :`,
               value: `${this.getLatest().stars.join(', ')}`,
+            },
+          ],
+        },
+        {
+          title: 'Expected next draw',
+          fields: [
+            {
+              name: `Balls :`,
+              value: `${nextDraw.balls.join(', ')}`,
+            },
+            {
+              name: `Stars :`,
+              value: `${nextDraw.stars.join(', ')}`,
             },
           ],
         },
